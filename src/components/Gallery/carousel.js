cee27bd70690227a8dbb2b950623b3551eec62ad
Joe <josephc.lambert@gmail.com>
2019-08-19 11:06:27 -0700

    starts hooks based implementation of carousel

--- src/components/Gallery/carousel.js
+++ src/components/Gallery/carousel.js
@@ -1,84 +1,47 @@
-import React, { Component } from 'react'
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 
-const StyledCarouselWrapper = styled.div`
-  .carousel.carousel-slider .control-arrow {
-    position: fixed;
-    z-index: 10;
-  }
-  .carousel {
-    .slider-wrapper {
-      position: fixed;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-    }
-
-    .slider {
-      display: flex;
-      flex-flow: row nowrap;
-    }
-
-    .slide {
-      height: 100vh;
-      min-width: 100%;
-      outline: 1px solid red;
-      position: relative;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: rgb(148, 0, 211);
-
+const StyledSliderWrapper = styled.section`
+  ul {
+    overflow: visible;
+    width: 100%;
+    li {
+      display: inline-block;
       img {
-        position: absolute;
-        max-height: 100%;
-        max-width: 90%;
       }
     }
   }
 `
 
-const MyCarouselWrapper = styled.div``
+let images = [
+  'https://via.placeholder.com/1600/1900',
+  'https://via.placeholder.com/900/1600',
+  'https://via.placeholder.com/1600/1900',
+  'https://via.placeholder.com/900/1600',
+  'https://via.placeholder.com/1600/1900',
+  'https://via.placeholder.com/900/1600',
+  'https://via.placeholder.com/1600/1900',
+  'https://via.placeholder.com/900/1600',
+  'https://via.placeholder.com/1600/1900',
+  'https://via.placeholder.com/900/1600',
+  'https://via.placeholder.com/1600/1900',
+  'https://via.placeholder.com/900/1600',
+]
 
-export default class SimpleSlider extends Component {
-  render() {
-    const RRCSettings = {
-      centerMode: true,
-      dynamicHeight: false,
-      emulateTouch: false,
-      useKeyboardArrows: true,
-      showThumbs: false,
-      showStatus: false,
-      showIndicators: false,
-      infiniteLoop: true,
-      autoPlay: false,
-    }
-
-    let images = [
-      'https://www.fillmurray.com/1600/1900',
-      'https://www.fillmurray.com/900/1600',
-      'https://www.fillmurray.com/1600/1900',
-      'https://www.fillmurray.com/900/1600',
-      'https://www.fillmurray.com/1600/1900',
-      'https://www.fillmurray.com/900/1600',
-      'https://www.fillmurray.com/1600/1900',
-      'https://www.fillmurray.com/900/1600',
-      'https://www.fillmurray.com/1600/1900',
-      'https://www.fillmurray.com/900/1600',
-      'https://www.fillmurray.com/1600/1900',
-      'https://www.fillmurray.com/900/1600',
-    ]
-    return (
-      <StyledCarouselWrapper>
-        <Carousel {...RRCSettings}>
-          {images.map((image, i) => {
-            return <img key={i} src={image} alt="dynamic alt text" visually-hide={true} />
-          })}
-        </Carousel>
-      </StyledCarouselWrapper>
-    )
-  }
+const SimpleSlider = () => {
+  return (
+    <StyledSliderWrapper>
+      <ul>
+        {images.map((image, i) => {
+          return (
+            <li key={i}>
+              <img src={image} alt="dynamic alt text" visually-hide={true} />
+            </li>
+          )
+        })}
+      </ul>
+    </StyledSliderWrapper>
+  )
 }
+
+export default SimpleSlider
